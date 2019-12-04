d6794d4a328b5e22d1b1e8f68456a0ac73464b11
frankymacster <fs.sunatori@me.com>
2019-12-03 20:11:05 -0500

    createReducer: refactor

--- src/createReducer.js
+++ src/createReducer.js
@@ -1,13 +1,9 @@
 // https://redux.js.org/recipes/reducing-boilerplate?source=post_page-----f560b2683b7b----------------------#generating-reducers
 
-function createReducer(initialState, handlers) {
-  return function reducer(state = initialState, action) {
-    if (handlers.hasOwnProperty(action.type)) {
-      return handlers[action.type](state, action);
-    } else {
-      return state;
-    }
-  };
-}
+const createReducer = (initialState, handlers) =>
+  (state = initialState, action) =>
+    handlers.hasOwnProperty(action.type)
+      ? handlers[action.type](state, action)
+      : state;
 
 export default createReducer;
