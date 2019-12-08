85007bb1616d572c76956a3864af78aef41bf07c
frankymacster <fs.sunatori@me.com>
2019-12-07 19:20:06 -0500

    Pomodoro: Timer: add pause <- add ToggleButton

---
+++ src/ToggleButton.js
@@ -0,0 +1,76 @@
+import React, { useReducer, useEffect } from "react";
+import createMachine from "./createMachine";
+
+
+const toggleButton = {
+  states: {
+    on: "on",
+    off: "off"
+  },
+  actions: {
+    onToggleRequested: "onToggleRequested",
+  }
+};
+toggleButton.initialState = {
+  type: toggleButton.states.off
+};
+toggleButton.transitions = createMachine(
+  // digraph G {
+  //   "off" -> "on" [ label="onToggleRequested" ]; 
+  //   "on" -> "off" [ label="onToggleRequested" ];
+  // }
+  toggleButton.initialState,
+  {
+    [toggleButton.states.on]: {
+      [toggleButton.actions.onToggleRequested]: state => ({
+        ...state,
+        type: toggleButton.states.off
+      })
+    },
+    [toggleButton.states.off]: {
+      [toggleButton.actions.onToggleRequested]: state => ({
+        ...state,
+        type: toggleButton.states.on
+      })
+    }
+  }
+);
+
+
+function ToggleButton({
+  turnOnText,
+  turnOffText,
+  toggleCondition,
+  onTurnedOn,
+  onTurnedOff
+}) {
+  [toggleButton.currentState, toggleButton.dispatch] = useReducer(toggleButton.transitions, toggleButton.initialState);
+
+  useEffect(() => {
+    if (toggleButton.currentState.type === toggleButton.states.on) {
+      onTurnedOn();
+    } else {
+      onTurnedOff();
+    }
+  }, [toggleButton.currentState]);
+
+  return (
+    <button
+      onClick={() => {
+        if (toggleCondition) {
+          toggleButton.dispatch({
+            type: toggleButton.actions.onToggleRequested
+          });
+        }
+      }}
+    >
+      {toggleButton.currentState.type === toggleButton.states.off
+        ? turnOnText
+        : turnOffText
+      }
+    </button>
+  );
+}
+
+
+export default ToggleButton;
