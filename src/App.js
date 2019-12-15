f23058912418d3690feed7f30b664a2c9a0cf18d
frankymacster <frankymacster@users.noreply.github.com>
2019-12-14 22:39:58 -0500

    serve layout on client instead of server
    to be able to deploy on netlify

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import { Switch, Route, Link, BrowserRouter } from "react-router-dom";
-import {
-  MuiThemeProvider,
-  AppBar
-} from "material-ui";
-import Button from '@material-ui/core/Button';
-import getData from "./getData";
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
+import { MuiThemeProvider, AppBar } from "material-ui";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
 import Toolbar from "@material-ui/core/Toolbar";
 import classList from "./classList";
 import Blocks from "./Blocks";
@@ -21,26 +17,29 @@ import Counter from "./Counter";
 import Pomodoro from "./Pomodoro";
 import BFS from "./BFS";
 import GraphDrawer from "./GraphDrawer";
+import layout from "./layout.yml";
+import jsyaml from "js-yaml";
 
-import './App.css';
+import "./App.css";
 
 const DataToComponent = {
   Pages: ({ root, key, params }) => (
     <>
       {DataToComponent.Header({ root, key, params })}
       <Switch>
-        {root && root.children.map(page => (
-          <Route
-            path={page.route}
-            render={() => (
-              <Blocks
-                root={[page]}
-                dataToComponent={DataToComponent}
-                params={params}
-              />
-            )}
-          />
-        ))}
+        {root &&
+          root.children.map(page => (
+            <Route
+              path={page.route}
+              render={() => (
+                <Blocks
+                  root={[page]}
+                  dataToComponent={DataToComponent}
+                  params={params}
+                />
+              )}
+            />
+          ))}
       </Switch>
     </>
   ),
@@ -48,10 +47,7 @@ const DataToComponent = {
     <AppBar
       showMenuIconButton={false}
       title={(location => (
-        <Tabs
-          className="container"
-          value={location.pathname}
-        >          
+        <Tabs className="container" value={location.pathname}>
           {root.children.map(page => (
             <Tab
               className="tab"
@@ -60,18 +56,15 @@ const DataToComponent = {
               to={page.route}
             />
           ))}
-        </Tabs>))(location)}
-      style={{ background: '#1AB394' }}
+        </Tabs>
+      ))(location)}
+      style={{ background: "#1AB394" }}
       iconClassNameRight="muidocs-icon-navigation-expand-more"
     />
   ),
   Footer: ({ root, key, params }) => {
     return (
-      <AppBar
-        position="absolute"
-        className="footer"
-        showMenuIconButton={false}
-      >
+      <AppBar position="absolute" className="footer" showMenuIconButton={false}>
         <Toolbar>
           <Blocks
             key={key}
@@ -90,33 +83,29 @@ const DataToComponent = {
         "horizontal-center": root.align === "center"
       })}
     >
-      {root.children &&
+      {root.children && (
         <Blocks
           key={key}
           root={root.children}
           dataToComponent={DataToComponent}
           params={params}
-        />}
+        />
+      )}
     </div>
   ),
   ColumnChildren: ({ root, key, params }) => (
-    <div
-      className="column-children"
-    >
-      {root.children &&
+    <div className="column-children">
+      {root.children && (
         <Blocks
           key={key}
           root={root.children}
           dataToComponent={DataToComponent}
           params={params}
-        />}
+        />
+      )}
     </div>
   ),
-  Text: ({ root }) => (
-    <div>
-      {root.text}
-    </div>
-  ),
+  Text: ({ root }) => <div>{root.text}</div>,
   MediaCard: ({ root, root: { title, text } }) => (
     <Card className="media-card">
       <CardActionArea>
@@ -128,13 +117,11 @@ const DataToComponent = {
           <Typography gutterBottom variant="h5" component="h2">
             {title}
           </Typography>
-          <Typography component="p">
-            {text}
-          </Typography>
+          <Typography component="p">{text}</Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
-        {root.codelink &&
+        {root.codelink && (
           <Button
             size="small"
             color="primary"
@@ -142,8 +129,9 @@ const DataToComponent = {
             href={root.codelink}
           >
             Code
-          </Button>}
-        {root.demolink &&
+          </Button>
+        )}
+        {root.demolink && (
           <Button
             size="small"
             color="primary"
@@ -151,7 +139,8 @@ const DataToComponent = {
             href={root.demolink}
           >
             Demo
-          </Button>}
+          </Button>
+        )}
       </CardActions>
     </Card>
   ),
@@ -166,11 +155,7 @@ const DataToComponent = {
     </Button>
   ),
   Link: ({ root }) => (
-    <Link
-      className="link"
-      target="_blank"
-      href={root.link}
-    >
+    <Link className="link" target="_blank" href={root.link}>
       {root.text}
     </Link>
   ),
@@ -178,19 +163,23 @@ const DataToComponent = {
     <input
       value={state.text}
       type="text"
-      onChange={e => dispatch({
-        type: "setText",
-        text: parseInt(e.target.value)
-      })}
+      onChange={e =>
+        dispatch({
+          type: "setText",
+          text: parseInt(e.target.value)
+        })
+      }
     />
   ),
   Counter: ({ root: { initialCount }, params: { state, dispatch } }) => (
     <Counter
       initialCount={state.text}
-      onChange={s => dispatch({
-        type: "setText",
-        text: s.count
-      })}
+      onChange={s =>
+        dispatch({
+          type: "setText",
+          text: s.count
+        })
+      }
     />
   ),
   Pomodoro: () => (
@@ -207,18 +196,14 @@ const DataToComponent = {
     <Card className="media-card">
       <GraphDrawer />
     </Card>
-  ),
+  )
 };
 
 function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    const doEffect = async () => {
-      setData(await getData("/api/data"));
-    };
-
-    doEffect();
+    setData(jsyaml.load(layout));
   }, []);
 
   return (
@@ -228,7 +213,7 @@ function App() {
           path="/"
           render={({ location }) => (
             <MuiThemeProvider>
-              {data &&
+              {data && (
                 <Blocks
                   dataToComponent={DataToComponent}
                   root={data}
@@ -236,7 +221,7 @@ function App() {
                     location
                   }}
                 />
-              }
+              )}
             </MuiThemeProvider>
           )}
         />
